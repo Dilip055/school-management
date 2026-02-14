936e00be807cdf6559f2743d0f0c7798470af9ab
Dilip055 <dilipsuthar055@gmail.com>
2026-02-14 11:37:50 +0530

    add db

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS || '',
+  process.env.DB_PASS !== undefined ? process.env.DB_PASS : '',
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 4000,   // ⭐ TiDB uses port 4000
